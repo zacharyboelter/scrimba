80881f5c628b547faf9ccbf945d8cef7438e581d
Zachary Boelter <zboelter@gmail.com>
2022-02-16 12:57:32 -0600

    console increment function

--- app.js
+++ app.js
@@ -9,8 +9,14 @@
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
 
+
+let count = 0
+
 function increment() {
-    console.log('the button was clicked')
+    count = count + 1;
+    console.log(count);
 }
+
+
 
 
